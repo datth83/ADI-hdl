RHSResearchLLC/hdl

daq2/intel: Modify interconnect architecture
In case of Nios II based system the main memory interconnect can grow
significantly, because it has to handle all the peripherals and devices
which needs access the memory.

To decrease the size of the interconnect, introduce new secondary Avalon
memory mapped bridges between the main interconnect and peripherals.

The following bridges were introduced:

 - avl_mm_peripheral_bridge@0x10050000 - connects all the base design's
   peripherals
 - avl_mm_bridge_tx@0x10060000 - connects all the Avalon interfaces from the TX
   path
 - avl_mm_bridge_rx@0x10068000 - connects all the Avalon interfaces from the RX
   path
 - axi_mm_bridge_0@0x10070000 - connects all the AXI interfaces from TX
   and RX path

Signed-off-by: Dan Hotoleanu <dan.hotoleanu@analog.com>
Signed-off-by: Istvan Csomortani <istvan.csomortani@analog.com>

projects/daq2/common/daq2_qsys.tcl
@@ -205,26 +205,26 @@ for {set i 0} {$i < $TX_NUM_OF_LANES} {incr i} {
 
 # addresses
 
-ad_cpu_interconnect 0x00020000 ad9144_jesd204.link_reconfig
-ad_cpu_interconnect 0x00024000 ad9144_jesd204.link_management
-ad_cpu_interconnect 0x00025000 ad9144_jesd204.link_pll_reconfig
-ad_cpu_interconnect 0x00026000 ad9144_jesd204.lane_pll_reconfig
-ad_cpu_interconnect 0x00028000 avl_adxcfg_0.rcfg_s0
-ad_cpu_interconnect 0x00029000 avl_adxcfg_1.rcfg_s0
-ad_cpu_interconnect 0x0002a000 avl_adxcfg_2.rcfg_s0
-ad_cpu_interconnect 0x0002b000 avl_adxcfg_3.rcfg_s0
-ad_cpu_interconnect 0x0002c000 axi_ad9144_dma.s_axi
-ad_cpu_interconnect 0x00034000 axi_ad9144.s_axi
+ad_cpu_interconnect 0x00000000 ad9144_jesd204.link_reconfig     "axi_mm_bridge_0"  0x10080000
+ad_cpu_interconnect 0x00004000 ad9680_jesd204.link_reconfig     "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00008000 ad9144_jesd204.link_management   "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00009000 ad9680_jesd204.link_management   "axi_mm_bridge_0"
+ad_cpu_interconnect 0x0000A000 axi_ad9144.s_axi                 "axi_mm_bridge_0"
+ad_cpu_interconnect 0x0000E000 axi_ad9680.s_axi                 "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00012000 axi_ad9144_dma.s_axi             "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00012800 axi_ad9680_dma.s_axi             "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00000000 ad9144_jesd204.link_pll_reconfig "avl_mm_bridge_tx" 0x10060000
+ad_cpu_interconnect 0x00001000 ad9144_jesd204.lane_pll_reconfig "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00002000 avl_adxcfg_0.rcfg_s0             "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00003000 avl_adxcfg_1.rcfg_s0             "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00004000 avl_adxcfg_2.rcfg_s0             "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00005000 avl_adxcfg_3.rcfg_s0             "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00000000 ad9680_jesd204.link_pll_reconfig "avl_mm_bridge_rx" 0x10068000
+ad_cpu_interconnect 0x00001000 avl_adxcfg_0.rcfg_s1             "avl_mm_bridge_rx"
+ad_cpu_interconnect 0x00002000 avl_adxcfg_1.rcfg_s1             "avl_mm_bridge_rx"
+ad_cpu_interconnect 0x00003000 avl_adxcfg_2.rcfg_s1             "avl_mm_bridge_rx"
+ad_cpu_interconnect 0x00004000 avl_adxcfg_3.rcfg_s1             "avl_mm_bridge_rx"
 
-ad_cpu_interconnect 0x00040000 ad9680_jesd204.link_reconfig
-ad_cpu_interconnect 0x00044000 ad9680_jesd204.link_management
-ad_cpu_interconnect 0x00045000 ad9680_jesd204.link_pll_reconfig
-ad_cpu_interconnect 0x00048000 avl_adxcfg_0.rcfg_s1
-ad_cpu_interconnect 0x00049000 avl_adxcfg_1.rcfg_s1
-ad_cpu_interconnect 0x0004a000 avl_adxcfg_2.rcfg_s1
-ad_cpu_interconnect 0x0004b000 avl_adxcfg_3.rcfg_s1
-ad_cpu_interconnect 0x0004c000 axi_ad9680_dma.s_axi
-ad_cpu_interconnect 0x00050000 axi_ad9680.s_axi
 
 # dma interconnects
 

datth83/ADI-hdl

projects/daq2/common/daq2_qsys.tcl
@@ -205,26 +205,26 @@ for {set i 0} {$i < $TX_NUM_OF_LANES} {incr i} {
 
 # addresses
 
-ad_cpu_interconnect 0x00020000 ad9144_jesd204.link_reconfig
-ad_cpu_interconnect 0x00024000 ad9144_jesd204.link_management
-ad_cpu_interconnect 0x00025000 ad9144_jesd204.link_pll_reconfig
-ad_cpu_interconnect 0x00026000 ad9144_jesd204.lane_pll_reconfig
-ad_cpu_interconnect 0x00028000 avl_adxcfg_0.rcfg_s0
-ad_cpu_interconnect 0x00029000 avl_adxcfg_1.rcfg_s0
-ad_cpu_interconnect 0x0002a000 avl_adxcfg_2.rcfg_s0
-ad_cpu_interconnect 0x0002b000 avl_adxcfg_3.rcfg_s0
-ad_cpu_interconnect 0x0002c000 axi_ad9144_dma.s_axi
-ad_cpu_interconnect 0x00034000 axi_ad9144.s_axi
+ad_cpu_interconnect 0x00000000 ad9144_jesd204.link_reconfig     "axi_mm_bridge_0"  0x00080000
+ad_cpu_interconnect 0x00004000 ad9680_jesd204.link_reconfig     "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00008000 ad9144_jesd204.link_management   "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00009000 ad9680_jesd204.link_management   "axi_mm_bridge_0"
+ad_cpu_interconnect 0x0000A000 axi_ad9144.s_axi                 "axi_mm_bridge_0"
+ad_cpu_interconnect 0x0000E000 axi_ad9680.s_axi                 "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00012000 axi_ad9144_dma.s_axi             "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00012800 axi_ad9680_dma.s_axi             "axi_mm_bridge_0"
+ad_cpu_interconnect 0x00000000 ad9144_jesd204.link_pll_reconfig "avl_mm_bridge_tx" 0x00060000
+ad_cpu_interconnect 0x00001000 ad9144_jesd204.lane_pll_reconfig "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00002000 avl_adxcfg_0.rcfg_s0             "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00003000 avl_adxcfg_1.rcfg_s0             "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00004000 avl_adxcfg_2.rcfg_s0             "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00005000 avl_adxcfg_3.rcfg_s0             "avl_mm_bridge_tx"
+ad_cpu_interconnect 0x00000000 ad9680_jesd204.link_pll_reconfig "avl_mm_bridge_rx" 0x00068000
+ad_cpu_interconnect 0x00001000 avl_adxcfg_0.rcfg_s1             "avl_mm_bridge_rx"
+ad_cpu_interconnect 0x00002000 avl_adxcfg_1.rcfg_s1             "avl_mm_bridge_rx"
+ad_cpu_interconnect 0x00003000 avl_adxcfg_2.rcfg_s1             "avl_mm_bridge_rx"
+ad_cpu_interconnect 0x00004000 avl_adxcfg_3.rcfg_s1             "avl_mm_bridge_rx"
 
-ad_cpu_interconnect 0x00040000 ad9680_jesd204.link_reconfig
-ad_cpu_interconnect 0x00044000 ad9680_jesd204.link_management
-ad_cpu_interconnect 0x00045000 ad9680_jesd204.link_pll_reconfig
-ad_cpu_interconnect 0x00048000 avl_adxcfg_0.rcfg_s1
-ad_cpu_interconnect 0x00049000 avl_adxcfg_1.rcfg_s1
-ad_cpu_interconnect 0x0004a000 avl_adxcfg_2.rcfg_s1
-ad_cpu_interconnect 0x0004b000 avl_adxcfg_3.rcfg_s1
-ad_cpu_interconnect 0x0004c000 axi_ad9680_dma.s_axi
-ad_cpu_interconnect 0x00050000 axi_ad9680.s_axi
 
 # dma interconnects
 